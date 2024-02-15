e3f710144c09cca4ff3528984d6abcc0669530a8
reidcook <90933623+reidcook@users.noreply.github.com>
2024-02-15 14:07:38 -0500

    basic flask/sqlite3 implementation

---
+++ my_site.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import render_template
+from flask import g
+import sqlite3
+
+app = Flask(__name__)
+
+DATABASE = 'names.db'
+
+def get_db():
+        db = getattr(g, '_database,', None)
+        if db is None:
+            db = g._database = sqlite3.connect(DATABASE)
+            cursor = db.cursor()
+            cursor.execute("select * from tbl1")
+        return cursor.fetchall()
+
+@app.route("/")
+def index(name=None):
+        data = get_db()
+        return str(data)
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
