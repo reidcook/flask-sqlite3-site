3751a8eca81a70fcdbe344e215a6a369485d566d
reidcook <reidcook41@gmail.com>
2024-02-23 15:59:17 -0500

    added delete user, and search user functions

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-hw2 folder includes a vitual python env
+Install any version of python3, i had python 3.9.7
+pip install Flask
+
+Then in the folder with the my_site.py file run:
+flask --app my_site run --debug
+
+Then connect to the site with 127.0.0.1:5000
--- my_site.py
+++ my_site.py
@@ -4,18 +4,19 @@ from flask import g
 from flask import request
 from flask import redirect
 from flask import abort
+from flask import url_for
 import sqlite3
 
 app = Flask(__name__)
 
-DATABASE = 'names.db'
+DATABASE = 'UserDatabase.db'
 
 def get_data():
         db = getattr(g, '_database,', None)
         if db is None:
             db = g._database = sqlite3.connect(DATABASE)
             cursor = db.cursor()
-            cursor.execute("select * from tbl1")
+            cursor.execute("select * from Users")
         return cursor.fetchall()
 def get_db():
     db = getattr(g, '_database,', None)
@@ -25,7 +26,7 @@ def get_db():
 
 def get_post(post_id):
     conn = get_db()
-    post = conn.execute('SELECT * FROM tbl1 WHERE id = ?',
+    post = conn.execute('SELECT * FROM Users WHERE Id = ?',
                         (post_id,)).fetchone()
     conn.close()
     if post is None:
@@ -35,7 +36,12 @@ def get_post(post_id):
 @app.route("/", methods=["GET"])
 def index(name=None):
         data = get_data()
-        return render_template("index.html", all_data=data)
+        return render_template("index.html", all_data=data, selected_user=("", "", ""))
+@app.route("/<user>", methods=["GET"])
+def userSearch(user):
+        the_user = get_post(user)
+        data = get_data()
+        return render_template("index.html", all_data=data, selected_user=the_user)
 @app.route("/submituser", methods=["POST"])
 def add(name=None):
         app.logger.info('testing info log')
@@ -43,15 +49,14 @@ def add(name=None):
             db = getattr(g, '_database,', None)
             db = g._database = sqlite3.connect(DATABASE)
             cursor = db.cursor()
-            newUser = (request.form['name'], int(request.form['id']), int(request.form['points']))
+            newUser = (int(request.form['id']), request.form['name'], int(request.form['points']))
             try:
-                cursor.execute("INSERT INTO tbl1 VALUES (?, ?, ?)", newUser)
+                cursor.execute("INSERT INTO Users VALUES (?, ?, ?)", newUser)
                 db.commit()
                 db.close()
-                app.logger.info("hello")
                 return redirect("/")
             except:
-                return "There was an error that occured"
+                return redirect("/")
             
 @app.route("/edituser", methods=["POST"])
 def edit():
@@ -61,13 +66,32 @@ def edit():
             cursor = db.cursor()
             newUser = (request.form['name'], int(request.form['id']), int(request.form['points']), int(request.form['edit_user']))
             try:
-                cursor.execute("UPDATE tbl1 SET Name = ?, Id = ?, Points = ?" "WHERE Id = ?", newUser)
+                cursor.execute("UPDATE Users SET Name = ?, Id = ?, Points = ?" "WHERE Id = ?", newUser)
                 db.commit()
                 db.close()
-                app.logger.info("hello")
                 return redirect("/")
             except:
-                return "There was an error that occured"
+                return redirect("/")
+@app.route("/deleteuser", methods=["POST"])
+def delete():
+     if request.method == "POST":
+            db = getattr(g, '_database,', None)
+            db = g._database = sqlite3.connect(DATABASE)
+            cursor = db.cursor()
+            newUser = (request.form['deleted_user'])
+            try:
+                cursor.execute("DELETE FROM Users WHERE Id = ?", newUser)
+                db.commit()
+                db.close()
+                return redirect("/")
+            except:
+                return redirect("/")
+@app.route("/searchuser", methods=["POST"])
+def search():
+     if request.method == "POST":
+            searched_user = request.form['searched_user']
+            app.logger.info('testing info log ' + searched_user)
+            return redirect("/"+searched_user)
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
